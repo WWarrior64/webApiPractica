f2c10c6c51bd2f65452d2ad4f388deaaf05954ee
WWarrior64 <115961340+WWarrior64@users.noreply.github.com>
2025-02-13 08:22:15 -0600

    Practica 3, desarrollo de la practica hasta la parte de TOP N

--- webApiPractica/Controllers/equiposController.cs
+++ webApiPractica/Controllers/equiposController.cs
@@ -24,7 +24,23 @@ namespace webApiPractica.Controllers
 		[Route("GetAll")]
 		public IActionResult Get()
 		{
-			List<equipos> listadoEquipo = (from e in _equiposContexto.equipos select e).ToList();
+			var listadoEquipo = (from e in _equiposContexto.equipos join t in _equiposContexto.tipo_equipo 
+										   on e.tipo_equipo_id equals t.id_tipo_equipo join m in _equiposContexto.marcas 
+										   on e.marca_id equals m.id_marcas join es in _equiposContexto.estados_equipo 
+										   on e.estado_equipo_id equals es.id_estados_equipo select new
+										   {
+											   e.id_equipos,
+											   e.nombre,
+											   e.descripcion,
+											   e.tipo_equipo_id,
+											   tipo_equipo = t.descripcion,
+											   e.marca_id,
+											   marca = m.nombre_marca,
+											   e.estado_equipo_id,
+											   estado_equipo = es.descripcion,
+											   detalle = $"Tipo: { t.descripcion }, Marca { m.nombre_marca } Estado Equipo { es.descripcion }",
+											   e.estado
+										   }).Take(1).ToList();
 
 			if (listadoEquipo.Count == 0) 
 			{
--- webApiPractica/Models/equiposContext.cs
+++ webApiPractica/Models/equiposContext.cs
@@ -10,5 +10,8 @@ namespace webApiPractica.Models
 		}
 
 		public DbSet<equipos> equipos { get; set; }
+		public DbSet<marcas> marcas { get; set; }
+		public DbSet<tipo_equipo> tipo_equipo { get; set; }
+		public DbSet<estados_equipo> estados_equipo { get; set; }
 	}
 }
